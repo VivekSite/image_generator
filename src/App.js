325869a6f994e8a8ffd1f95514e2f517df10bee5
Vivek_Sahani <sram39266@gmail.com>
2023-10-01 17:12:47 +0530

    options updated

--- src/App.js
+++ src/App.js
@@ -44,14 +44,16 @@ function App() {
           placeholder="Search To Get Images"
         />
         <select name='orientation' value={orientation} onChange={e => setOrientation(e.target.value)}>
-          <option value='square' selected>Square</option>
+          <option value='' selected>orientation</option>
+          <option value='square'>Square</option>
           <option value='landscape' >Landscape</option>
           <option value='portrait' >Portrait</option>
         </select>
 
         <select name='size' value={size} onChange={e => setSize(e.target.value)}>
+          <option value='' selected>Size</option>
           <option value='small'>Small</option>
-          <option value='medium' selected>Medium</option>
+          <option value='medium'>Medium</option>
           <option value='large'>Large</option>
         </select>
         <button onClick={handle_image_generation}>generate</button>
