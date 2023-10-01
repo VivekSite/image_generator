ef508afcd7f1f9094cc0bd7d2063ea0682c51577
Vivek_Sahani <sram39266@gmail.com>
2023-10-01 16:51:47 +0530

    image generator in ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [query, setQuery] = useState();
+  const [orientation, setOrientation] = useState();
+  const [size, setSize] = useState();
+
+  const api_key = "mAb59WInPy7nbV7ziIGPDkyPKK0j4G2ZOL2toiRiyjrbqvf1koaV66y6";
+  const per_page = 10;
+
+  const handle_image_generation = () => {
+    fetch(
+      `https://api.pexels.com/v1/search?query=${query}&per_page=${per_page}&size=${size}&orientation=${orientation}`,
+      {
+        headers: {
+          Authorization: api_key,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const images = data.photos;
+        const imageContainer = document.getElementById("image-container");
+  
+        images.forEach((image) => {
+          const imgElement = document.createElement("img");
+          imgElement.src = image.src.medium;
+          imageContainer.appendChild(imgElement);
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching images " + error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="searchArea">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search To Get Images"
+        />
+        <select name='orientation' value={orientation} onChange={e => setOrientation(e.target.value)}>
+          <option value='square' selected>Square</option>
+          <option value='landscape' >Landscape</option>
+          <option value='portrait' >Portrait</option>
+        </select>
+
+        <select name='size' value={size} onChange={e => setSize(e.target.value)}>
+          <option value='small'>Small</option>
+          <option value='medium' selected>Medium</option>
+          <option value='large'>Large</option>
+        </select>
+        <button onClick={handle_image_generation}>generate</button>
+      </div>
+      <div id="image-container">
+
+      </div>
+    </>
   );
 }
 
